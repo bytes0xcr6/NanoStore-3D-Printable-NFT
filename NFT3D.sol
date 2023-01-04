e6d9e67a3e8851a2437dee930188f276bee4f4fe
CristianRicharte6 <102038261+CristianRicharte6@users.noreply.github.com>
2023-01-04 17:10:18 +0100

    Update NFT3D.sol

--- NFT3D.sol
+++ NFT3D.sol
@@ -3,18 +3,18 @@
 pragma solidity 0.8.17;
 
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 
+contract NFT3D is ERC1155 {
 
-contract NFT3D is ERC1155, Ownable {
-
-    event URI3DBurned(address indexed Owner, string URI, uint Size, uint BurningTime);
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+    event URI3DBurned(address indexed Owner, uint NFTCollection, string URI, uint Size, string Material, string Store, uint BurningTime);
     event NFTMinted(address indexed Owner, uint NFTCollection, uint Amount, uint MintingTime);
     event URIUpdated(uint NFTCollection, string NewURI, uint UpdateTime);
 
 
     // 1st NFT collection will be number 0 by default.
     uint private NFTcount;
+    address public owner;
 
     // NFTidCollection -> URIs array. (It will contain STL file & 3D details choosen by the user).
     mapping(uint => string) private UriToPrint;
@@ -27,21 +27,39 @@ contract NFT3D is ERC1155, Ownable {
     // NFTidCollection -> Request URI -> owner or creator -> Accepted.
     mapping(uint => mapping(string => mapping(address => bool))) changeURIRequest;
 
-    constructor(string memory _uri) ERC1155(_uri){
-
+    /**
+     * @dev Throws if called by any account other than the owner.
+     */
+    modifier onlyOwner() {
+        require(owner == msg.sender, "Caller is not the owner");
+        _;
     }
 
-    // Burning function to burn the token and emit the URI to print.
-    function printNFT(uint _NFTCollection, uint _amount, uint _size) public returns(bool){
+    constructor() ERC1155(""){}
+
+    /**
+     * @dev Burning function to burn the token and emit the URI to print.
+     *      If we choose more than 1 NFT, it will print all of them under the same conditions.
+     * @param _NFTCollection: NFT Collection Identifier.
+     * @param _amount: Total amount of NFTs we want to print. (Same NFT Collection)
+     * @param _size: Desirable size for the NFT printed.
+     * @param _material: Desirable material for the NFT printed.
+     * @param _store: Print store where we want to print the NFT.
+     */
+    function printNFT(uint _NFTCollection, uint _amount, uint _size, string memory _material, string memory _store) public returns(bool){
         _burn(msg.sender, _NFTCollection, _amount);
         string memory _uri = UriToPrint[_NFTCollection];
-        NFTsRemainingBurn[NFTcount] -= _amount;
+        NFTsRemainingBurn[_NFTCollection] -= _amount;
 
-        emit URI3DBurned(msg.sender, _uri, _size, block.timestamp);
+        emit URI3DBurned(msg.sender, _NFTCollection, _uri, _size, _material, _store, block.timestamp);
         return true;
     }
 
-    // Minting function. It will set the URI, NFT Collection & amount of NFTs created.
+    /**
+     * @dev Minting function. It will set the URI, NFT Collection & amount of NFTs created.
+     * @param _amount: Total amount of NFTs we want to mint. (Same NFT Collection)
+     * @param _uri: Uri we want to set as the default URI when burned.
+     */
     function mintNFT(uint _amount, string memory _uri) public returns(bool){
         _mint(msg.sender, NFTcount, _amount, "");
         NFTsMinted[NFTcount] = _amount;
@@ -55,12 +73,16 @@ contract NFT3D is ERC1155, Ownable {
         return true;
     }
 
-    // Setter for new URI. It needs the agreement of the contract Owner & NFT Creator.
+    /**
+     * @dev Setter for new URI. It needs the agreement of the contract Owner & NFT Creator.
+     * @param _NFTCollection: NFT Collection Identifier.
+     * @param _newURI: New URI we want to update for the NFT Collection ID.
+     */
     function updateURI(uint _NFTCollection, string memory _newURI) public returns(bool) {
-        require(msg.sender == owner() || CheckCreator[_NFTCollection] == msg.sender, "You can`t update URI");
+        require(msg.sender == owner || CheckCreator[_NFTCollection] == msg.sender, "You can`t update URI");
         require(!changeURIRequest[_NFTCollection][_newURI][msg.sender], "You already approved");
         changeURIRequest[_NFTCollection][_newURI][msg.sender] = true;
-        if(changeURIRequest[_NFTCollection][_newURI][owner()] && changeURIRequest[_NFTCollection][_newURI][CheckCreator[_NFTCollection]]) {
+        if(changeURIRequest[_NFTCollection][_newURI][owner] && changeURIRequest[_NFTCollection][_newURI][CheckCreator[_NFTCollection]]) {
             UriToPrint[_NFTCollection] = _newURI;
         }
 
@@ -68,7 +90,29 @@ contract NFT3D is ERC1155, Ownable {
         return true;
     }
 
+    /**
+     * @dev Transfers ownership of the contract to a new account (`newOwner`).
+     *      Can only be called by the current owner.
+     * @param newOwner: New Owner address we want to set.
+     */
+    function transferOwnership(address newOwner) public onlyOwner returns(bool){
+        require(newOwner != address(0), "New owner is the zero address");
+        address oldOwner = owner;
+        owner = newOwner;
+        emit OwnershipTransferred(oldOwner, newOwner);
+        return true;
+    }
 
+    /**
+     * @dev 
+     * @param   
+     */
+
+}
     // THINGS TO ADD: 
     // DOES IT NEED INTERFACE TO INTERACT WITH THE CONTRACT??
-}
+
+    /**
+     * @dev 
+     * @param   
+     */
